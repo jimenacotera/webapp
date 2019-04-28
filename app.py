c31ab8fb29d923d3641c79faf505cdc01b584e38
jimenacotera <jgc423@nyu.edu>
2019-04-28 13:41:59 -0400

    v2.1.1 funciona

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ try:
     from PIL import Image
 except ImportError:
     import Image
-import pytesseract
+#import pytesseract
 
-from picamera import PiCamera
+#from picamera import PiCamera
 import time
 
 
@@ -15,23 +15,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    value = capture()
-    #value = capture()
-    return render_template("index.html", val=value)
+    value = 'starter'
+    print(value)
+    return render_template("index.html", val = value)
 
-@app.route("/capture")
+@app.route("/capture", methods=['POST'])
 def capture():
+    '''
     with PiCamera() as camera:
         camera.start_preview()
 
         time.sleep(7)
         camera.capture('test.jpeg')
     name = pytesseract.image_to_string(Image.open('test.jpeg'))
+
     return(name)
 '''
-    val = 'unicorn'
+    val = 'cherry'
     return(val)
 
-'''
+
 
 app.run(port=8000, debug=True, host="0.0.0.0")
