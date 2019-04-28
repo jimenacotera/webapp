b462d0482d153a9240a21564d14c883eb4c01fd7
jimenacotera <47250699+jimenacotera@users.noreply.github.com>
2019-04-27 20:17:34 -0400

    v1

---
+++ app.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, render_template
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+#import pytesseract
+
+#from picamera import PiCamera
+import time
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    value = capture()
+    #value = capture()
+    return render_template("index.html", val=value)
+
+@app.route("/capture")
+def capture():
+    '''
+	with PiCamera() as camera:
+        camera.start_preview()
+        time.sleep(7)
+        camera.capture('test.jpeg')
+    name = pytesseract.image_to_string(Image.open('test.jpeg'))
+    return(name)
+    '''
+    val = 'unicorn'
+    return(val)
+
+
+
+app.run(port=8000, debug=True, host="0.0.0.0")
